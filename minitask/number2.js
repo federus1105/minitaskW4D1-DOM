24d6562c28c4ad4880d9847cfefeba11dcefdfbe
federus rudi <federusrudi@gmail.com>
2025-07-23 07:49:22 +0700

    fix: file number2.js

--- minitask/number2.js
+++ minitask/number2.js
@@ -1,14 +1,19 @@
-let catur = document.getElementById("catur");
+// let catur = document.getElementById("#catur");
+let papan = document.createElement("div");
+papan.className = "catur";
 
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; i++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
+for (let baris = 0; baris < 8; baris++) {
+  for (let kolom = 0; kolom < 8; kolom++) {
+    const box = document.createElement("div")
+    box.classList.add("box")
+
+    if((baris + kolom) % 2 === 0) {
+      box.classList.add("white")
+    } else {
+      box.classList.add("black")
+    }
+    papan.append(box)
   }
-  if ((i + j) % 2 === 0) {
-    box.classList.add("white");
-  } else {
-    box.classList.add("black");
-  }
-  catur.append(kotak)
 }
+
+document.body.append(papan);
